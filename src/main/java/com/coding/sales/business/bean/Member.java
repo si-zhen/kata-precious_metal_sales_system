d86ed6dafc35654bfe04b07739b9b7d0d26b0902
shisizhen <youheng100@163.com>
2019-07-02 19:20:45 +0800

    会员类信息修改

--- src/main/java/com/coding/sales/business/bean/Member.java
+++ src/main/java/com/coding/sales/business/bean/Member.java
@@ -1,20 +1,23 @@
 package com.coding.sales.business.bean;
 
+import com.coding.sales.business.constants.MemberLevel;
+
 /**
  * 会员信息JavaBean
+ * 
  * @author SK
  *
  */
 public class Member {
 
 	/**
-	 * 	会员姓名
+	 * 会员姓名
 	 */
 	private String name;
 	/**
 	 * 会员等级
 	 */
-	private String level;
+	private MemberLevel level;
 	/**
 	 * 会员卡号
 	 */
@@ -23,35 +26,45 @@ public class Member {
 	 * 会员积分
 	 */
 	private String point;
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getLevel() {
-		return level;
-	}
-	public void setLevel(String level) {
-		this.level = level;
-	}
+
 	public String getMemberId() {
 		return memberId;
 	}
+
 	public void setMemberId(String memberId) {
 		this.memberId = memberId;
 	}
+
 	public String getPoint() {
 		return point;
 	}
+
 	public void setPoint(String point) {
 		this.point = point;
 	}
-	
-	@Override
-	public String toString() {
-		return "Member [name=" + name + ", level=" + level + ", memberId=" + memberId + ", point=" + point + "]";
+
+	public MemberLevel getLevel() {
+		return level;
 	}
-	
+
+	public void setLevel(MemberLevel level) {
+		this.level = level;
+	}
+
+	public Member(String name, MemberLevel level, String memberId, String point) {
+		super();
+		this.name = name;
+		this.level = level;
+		this.memberId = memberId;
+		this.point = point;
+	}
+
 }
