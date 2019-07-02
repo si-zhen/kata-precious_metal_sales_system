66952da547bf540c190bf1984bc66e3f1f54937e
星星拥抱月亮 <sk120110>
2019-07-02 17:50:00 +0800

    修改9折95折打折方式，新增常量类

---
+++ src/main/java/com/coding/sales/business/constants/Constants.java
@@ -0,0 +1,50 @@
+package com.coding.sales.business.constants;
+
+/**
+ * 常量类
+ * @author SK
+ *
+ */
+public class Constants {
+
+	/**
+	 * 95折
+	 */
+	public static final String NINETYFIVE = "9.5";
+	/**
+	 * 9折
+	 */
+	public static final String NINETY = "9";
+	/**
+	 * 三千
+	 */
+	public static final int THREETHOUSAND = 3000;
+	/**
+	 * 两千
+	 */
+	public static final int TWOTHOUSAND = 2000;
+	/**
+	 * 一千
+	 */
+	public static final int ONETHOUSAND = 1000;
+	/**
+	 * 350
+	 */
+	public static final int THREEHUNDREDFIFTY = 350;
+	/**
+	 * 30
+	 */
+	public static final int THIRTY = 30;
+	/**
+	 * 10
+	 */
+	public static final int TEN = 10;
+	/**
+	 * 3
+	 */
+	public static final int THREE = 3;
+	/**
+	 * 4
+	 */
+	public static final int FOUR = 4;
+}
--- src/main/java/com/coding/sales/business/strategy/discount/DiscountNinety.java
+++ src/main/java/com/coding/sales/business/strategy/discount/DiscountNinety.java
@@ -5,18 +5,17 @@ import java.math.BigDecimal;
 import com.coding.sales.business.constants.Constants;
 
 /**
- * 95折券
+ * 9折券
  * 
- * @author Sizhen Created at 2019-07-02 17:04:41
+ * @author Sizhen Created at 2019-07-02 17:05:06
  */
-public class DiscountNinetyFive extends AbstractDiscountStrategy {
+public class DiscountNinety extends AbstractDiscountStrategy {
 
 	@Override
 	public BigDecimal calRealPay(BigDecimal totalMoney) {
-		if (totalMoney == null || totalMoney.compareTo(new BigDecimal("0")) < 0) {
+		if(totalMoney == null || totalMoney.compareTo(new BigDecimal("0")) < 0){
 			throw new RuntimeException("传入金额非法！");
 		}
-		
 		return totalMoney.multiply(new BigDecimal(Constants.NINETYFIVE));
 	}
 
