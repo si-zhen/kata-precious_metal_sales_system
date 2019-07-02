20fbb621fa0f9b9dbb8b7f2bca7994d065cc2b0b
shisizhen <youheng100@163.com>
2019-07-02 19:19:49 +0800

    会员信息数据、贵金属数据实现

---
+++ src/main/java/com/coding/sales/business/db/MemberInfo.java
@@ -0,0 +1,34 @@
+package com.coding.sales.business.db;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.coding.sales.business.bean.Member;
+import com.coding.sales.business.constants.MemberLevel;
+
+/**
+ * 会员信息
+ * 
+ * @author Sizhen Created at 2019-07-02 18:15:11
+ */
+public class MemberInfo {
+	private static Map<String, Member> infoMap = new HashMap<>();
+
+	static {
+		infoMap.put("6236609999", new Member("马丁", MemberLevel.NORMAL, "6236609999", "9860"));
+		infoMap.put("6630009999", new Member("王立", MemberLevel.GOLD, "6630009999", "48860"));
+		infoMap.put("8230009999", new Member("李想", MemberLevel.PLATINUM, "8230009999", "98860"));
+		infoMap.put("9230009999", new Member("张三", MemberLevel.DIAMONDS, "9230009999", "198860"));
+	}
+
+	/**
+	 * 根据会员id获取会员信息
+	 *
+	 * @param memberId
+	 *            会员id
+	 * @return 会员id对应的会员信息
+	 */
+	public static Member findMemberById(String memberId) {
+		return infoMap.get(memberId);
+	}
+}
---
+++ src/main/java/com/coding/sales/business/db/MetalInfo.java
@@ -0,0 +1,55 @@
+package com.coding.sales.business.db;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.coding.sales.business.bean.Metal;
+import com.coding.sales.business.strategy.discount.DiscountNinety;
+import com.coding.sales.business.strategy.discount.DiscountNinetyFive;
+import com.coding.sales.business.strategy.reduction.ReductionOverFourGiveOne;
+import com.coding.sales.business.strategy.reduction.ReductionOverOneThousand;
+import com.coding.sales.business.strategy.reduction.ReductionOverThreeGiveHalf;
+import com.coding.sales.business.strategy.reduction.ReductionOverThreeThousand;
+import com.coding.sales.business.strategy.reduction.ReductionOverTwoThousand;
+
+/**
+ * 贵金属商品信息
+ * 
+ * @author Sizhen Created at 2019-07-02 18:15:11
+ */
+public class MetalInfo {
+	private static Map<String, Metal> infoMap = new HashMap<>();
+	
+	static {
+		infoMap.put("001001", new Metal("001001", "册", "998.00", "世园会五十国钱币册"));
+		infoMap.put("001002",
+				new Metal("001002", "盒", "1380.00", "2019北京世园会纪念银章大全40g").addDiscountStrategy(new DiscountNinety()));
+		infoMap.put("003001",
+				new Metal("003001", "条", "1580.00", "招财进宝").addDiscountStrategy(new DiscountNinetyFive()));
+		infoMap.put("003002",
+				new Metal("003002", "条", "980.00", "水晶之恋").addReductionStrategy(new ReductionOverThreeGiveHalf())
+						.addReductionStrategy(new ReductionOverFourGiveOne()));
+		infoMap.put("002002",
+				new Metal("002002", "套", "998.00", "中国经典钱币套装").addReductionStrategy(new ReductionOverTwoThousand())
+						.addReductionStrategy(new ReductionOverOneThousand()));
+		infoMap.put("002001",
+				new Metal("002001", "条", "1080.00", "守扩之羽比翼双飞4.8g").addDiscountStrategy(new DiscountNinetyFive())
+						.addReductionStrategy(new ReductionOverThreeGiveHalf())
+						.addReductionStrategy(new ReductionOverFourGiveOne()));
+		infoMap.put("002003",
+				new Metal("002003", "套", "698.00", "中国银象棋12g ").addDiscountStrategy(new DiscountNinety())
+						.addReductionStrategy(new ReductionOverThreeThousand())
+						.addReductionStrategy(new ReductionOverTwoThousand())
+						.addReductionStrategy(new ReductionOverOneThousand()));
+	}
+
+	/**
+	 * 根据贵金属id获取贵金属商品信息
+	 *
+	 * @param metalId
+	 * @return
+	 */
+	public static Metal findMetalById(String metalId){
+		return infoMap.get(metalId);
+	}
+}
